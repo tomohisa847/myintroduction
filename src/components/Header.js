2a97350e78915af0e38a4933cbd4d8a26e79c52a
tomohisa847 <130384158+tomohisa847@users.noreply.github.com>
2025-07-17 03:03:40 +0900

    complite edit

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header(){
 	return(
 		<header style={{textAlign:'center',padding:'20px'}}>
 			<h1>自己紹介</h1>
-			<h2>公立諏訪東京理科大学 情報応用工学科 3年 田中智久</h2>
+			<h2>公立諏訪東京理科大学 情報応用工学科 4年 田中智久</h2>
 		</header>
 
 	);
